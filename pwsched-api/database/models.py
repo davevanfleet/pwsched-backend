4e294c20d380a559beecf65844dfbca63894e387
dvfleet413 <dvfleet413@gmail.com>
2020-08-22 23:48:47 -0400

    adding congregation model

--- pwsched-api/database/models.py
+++ pwsched-api/database/models.py
@@ -1,6 +1,10 @@
 from .db import db
 
 
+class Congregation(db.Document):
+    name = db.StringField(required=True)
+
+
 class Shift(db.Document):
     location = db.StringField(required=True)
     time = db.DateTimeField(required=True)
