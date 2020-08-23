dae17ec738c8d7c76e1d0ed1d933d6722cde79e6
dvfleet413 <dvfleet413@gmail.com>
2020-08-22 23:52:57 -0400

    added congregation routing

--- pwsched-api/resources/routes.py
+++ pwsched-api/resources/routes.py
@@ -1,6 +1,8 @@
-from .shift import ShiftApi, ShiftsApi
+from .shift import ShiftApi, ShiftsApi, CongregationApi, CongregationsApi
 
 
 def initialize_routes(api):
     api.add_resource(ShiftsApi, '/shifts')
     api.add_resource(ShiftApi, '/shifts/<id>')
+    api.add_resource(CongregationsApi, '/congregations')
+    api.add_resource(CongregationAPi, '/congregation/<id>')
