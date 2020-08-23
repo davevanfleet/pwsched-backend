949a56a9e5259d0e30b9fdcc72cf5d79d4387681
dvfleet413 <dvfleet413@gmail.com>
2020-08-22 23:51:12 -0400

    adding congregation resource

---
+++ pwsched-api/resources/congregation.py
@@ -0,0 +1,30 @@
+from flask import Response, request
+from flask_restful import Resource
+from database.models import Congregation
+
+
+class CongregationApi(Resource):
+    def get(self, id):
+        congregation = Congregation.objects().get(id=id).to_json()
+        return Response(congreagtion, mimetype="application/json", status=200)
+
+    def put(self, id):
+        body = request.get_json()
+        Congregation.objects().get(id=id).update(**body)
+        return '', 200
+
+    def delete(self, id):
+        Congregation.objects().get(id=id).delete()
+        return '', 200
+
+
+class CongregationsApi(Resource):
+    def get(self):
+        congregations = Congregation.objects().to_json()
+        return Response(congregations, mimetype="application/json", status=200)
+
+    def post(self):
+        body = request.get_json()
+        congregation = Congreagation(**body).save()
+        id = congregation.id
+        return {'id': str(id)}, 200
