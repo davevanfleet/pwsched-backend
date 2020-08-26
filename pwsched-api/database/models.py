546a3bea771fbe0c37b5cad6e5161e34096c4dc6
dvfleet413 <dvfleet413@gmail.com>
2020-08-26 10:52:26 -0400

    made requested_by field a reference field on Shfit model

--- pwsched-api/database/models.py
+++ pwsched-api/database/models.py
@@ -11,7 +11,7 @@ class Shift(db.Document):
     location = db.StringField(required=True)
     time = db.DateTimeField(required=True)
     volunteers = db.ListField(required=True, max_length=2)
-    requested_by = db.ListField()
+    requested_by = db.ListField(db.ReferenceField("User"))
 
 
 class Role(db.Document, RoleMixin):
