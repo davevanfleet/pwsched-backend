2090f3edf578f26d18e126ba5c498f7072da355f
davevanfleet <vanfleet@arsome.com>
2020-09-27 19:19:54 -0400

    added endpoint tests for Congregation blueprint

--- Tests/test_pwsched.py
+++ Tests/test_pwsched.py
@@ -1,5 +1,7 @@
 import os
 import pytest
+import json
+from time import sleep
 from datetime import datetime
 from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
@@ -70,8 +72,8 @@ def runner(app):
 
 
 def test_ping(client):
-    rv = client.get('/ping')
-    assert b'Hello, World!' in rv.data
+    response = client.get('/ping')
+    assert b'Hello, World!' in response.data
 
 
 # Model Tests
@@ -110,3 +112,49 @@ def test_create_user(client):
             .email == "fake2@fakemail.com")
     assert (User.objects().order_by('-id').first()
             .congregation.name == "English - Willimantic")
+
+
+# Endpoints Tests
+def test_get_congregations(client):
+    response = client.get('/congregations/')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data[0]['name'] == "English - Willimantic"
+
+
+def test_post_congregations(client):
+    payload = {"name": "Columbia"}
+    response = client.post('/congregations/',
+                           json=payload)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['name'] == "Columbia"
+    response = client.get('/congregations/')
+    data = response.get_json()
+    assert len(data) == 2
+    assert data[1]["name"] == "Columbia"
+
+
+def test_get_congregation(client):
+    cong_id = str(Congregation.objects().first().id)
+    response = client.get(f'/congregations/{cong_id}')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["name"] == "English - Willimantic"
+
+
+def test_put_congregation(client):
+    payload = {"name": "Not Willimantic"}
+    cong_id = str(Congregation.objects().first().id)
+    response = client.put(f'/congregations/{cong_id}',
+                          json=payload)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["name"] == "Not Willimantic"
+
+
+def test_delete_congregation(client):
+    cong_id = str(Congregation.objects().first().id)
+    response = client.delete(f'/congregations/{cong_id}')
+    assert response.status_code == 200
+    assert len(Congregation.objects()) == 0
